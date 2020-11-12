84141100a926548f2d6b788508e9b257ef299db4
mavrenkov <mawro@bk.ru>
2020-11-12 13:45:34 -0600

    conflict resolved

--- src/test/java/com/vytrack/runner/CucumberRunner.java
+++ src/test/java/com/vytrack/runner/CucumberRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features="src/test/resources/features",
         glue="com/vytrack/stepDefinitions",
-        tags="@TC3",
+        tags="@TC4",
         dryRun = false
 
 
