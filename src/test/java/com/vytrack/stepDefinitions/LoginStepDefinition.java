d9f08177be6deb92b95f92be953cc7e2691a0149
Nurbiye <nurbiye720@gmail.com>
2020-11-11 19:34:24 -0600

    Setup commit

---
+++ src/test/java/com/vytrack/stepDefinitions/LoginStepDefinition.java
@@ -0,0 +1,25 @@
+package com.vytrack.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinition {
+    @Given("user on the login page")
+    public void user_on_the_login_page() {
+
+    }
+
+
+
+    @When("user logs in as a {string}")
+    public void user_logs_in_as_a(String string) {
+
+    }
+
+    @Then("user should see Dashboard page")
+    public void user_should_see_dashboard_page() {
+
+    }
+
+}
